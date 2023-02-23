e3b20cd63c07e1f0ff0f53929c5f0211ee68e7d8
camiyuka <camila.yatabe@gmail.com>
2023-02-22 22:29:24 -0300

    começo do JS

---
+++ JS/projeto.js
@@ -0,0 +1,7 @@
+document.getElementById("nome do botão aqui")
+document.getElementById("nome do botão aqui").onclick =
+ function() { /*aqui vai ser a função do botão-copiar e colar o html do nosso site, para o html 
+ do site já criado la mas o resto n sei como q faz, acho que precisa do html mesmo*/
+ 
+
+}
