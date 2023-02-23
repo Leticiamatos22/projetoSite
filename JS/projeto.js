ab4291f1d4b192608056ba84e4805f0081ce16c0
camiyuka <camila.yatabe@gmail.com>
2023-02-22 22:33:53 -0300

    btn gerar

--- JS/projeto.js
+++ JS/projeto.js
@@ -1,5 +1,5 @@
-document.getElementById("nome do botão aqui")
-document.getElementById("nome do botão aqui").onclick =
+document.getElementById("gerar")
+document.getElementById("gerar").onclick =
  function() { /*aqui vai ser a função do botão-copiar e colar o html do nosso site, para o html 
  do site já criado la mas o resto n sei como q faz, acho que precisa do html mesmo*/
  
