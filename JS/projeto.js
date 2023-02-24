4595eb7abaaf93b04bcea5b7c6588118710febbd
Leticiamatos22 <cristinadematosleticia@gmail.com>
2023-02-24 19:25:38 -0300

    seven

--- JS/projeto.js
+++ JS/projeto.js
@@ -1,7 +1,19 @@
-document.getElementById("gerar")
-document.getElementById("gerar").onclick =
- function() { /*aqui vai ser a função do botão-copiar e colar o html do nosso site, para o html 
- do site já criado la mas o resto n sei como q faz, acho que precisa do html mesmo*/
- 
+var btn = document.getElementById('gerar')
+btn.addEventListener('click',inserir)
 
+function inserir(){
+    //ocultar pagina 1 e exibir a pagina 2
+    let p1 = document.getElementById('page1')
+    let p2 = document.getElementById('page2')
+
+    p1.style.display = "none"
+    p2.style.display = "flex"
+
+    //inserir o codigo em html
+    let codeHtml = document.getElementById('codeHtml').value 
+    p2.innerHTML = codeHtml
+
+    //inserir o codigo em js
+    let codeJs = document.getElementById('codeJs').value 
+    document.getElementById('jsUser').innerHTML = codeJs
 }
