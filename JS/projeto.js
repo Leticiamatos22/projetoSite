590cdb9ee95287ac791bc7a28c04c0b060e1510c
Leticiamatos22 <cristinadematosleticia@gmail.com>
2023-02-24 20:47:43 -0300

    ultimo

--- JS/projeto.js
+++ JS/projeto.js
@@ -16,4 +16,7 @@ function inserir(){
     //inserir o codigo em js
     let codeJs = document.getElementById('codeJs').value 
     document.getElementById('jsUser').innerHTML = codeJs
+
+    document.getElementById("link").setAttribute("href", "/CSS/style2.css")
+
 }
